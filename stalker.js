898f56830e38d411b448470ef69e44adeb861b74
Anthony Spencer <33986201+SPEN84@users.noreply.github.com>
2019-04-13 14:40:40 -0400

    Add files via upload

---
+++ stalker.js
@@ -0,0 +1,7 @@
+var whatFirst = prompt("What is your first name?");
+var whatLast = prompt("What is your last name?");
+var whatAge = prompt("What is your age?");
+
+
+
+alert(whatFirst[0].toUpperCase() + " " + whatLast[0].toUpperCase() + " is " + whatAge + " years old."); 
